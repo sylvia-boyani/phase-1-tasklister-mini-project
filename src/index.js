7c7d93faadb72a503f95ff7fd778666226b09f86
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-15 11:10:32 +0300

    todo form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_todo.value)
+    form.reset()
+  })
+})
+
+function buildToDo(todo){
+let p = document.createElement("p")
+let btn = document.createElement("button")
+btn.addEventListener("click", handleDelete)
+btn.textContent = "x"
+p.textContent = `${todo}`
+p.appendChild(btn)
+console.log(p)
+document.querySelector("#todo_container").appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
